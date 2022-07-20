38ad8967d813ff4bbde9499f40e93149904f3692
dunnyk <danielkamau231@gmail.com>
2022-07-21 01:27:38 +0300

    Update the state in react

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,4 +1,3 @@
-import { timers } from "jquery";
 import React, { Component } from "react";
 import "./index.css";
 
@@ -10,29 +9,34 @@ export default class MainContent extends Component {
       {
         id: 1,
         names: "Arrow Dan",
-        age: 23,
+        age: "",
         school: "Versity of Birmigham",
+        photo: "https://picsum.photos/id/237/50/50",
       },
       {
         id: 2,
-        names: "Ashley Mazuri",
-        age: "",
+        names: "Eshley Mazuri",
+        age: 576,
         school: "Versity of naironi",
+        photo: "https://picsum.photos/id/238/50/50",
       },
       {
         id: 3,
         names: "Kip kemboi",
-        age: 38,
+        age: "",
         school: "Couraage of amichah",
+        photo: "https://picsum.photos/id/239/50/50",
       },
       {
         id: 4,
         names: "Mikah Koroso",
-        age: "",
+        age: 35678,
         school: "Not seen a class door",
+        photo: "https://picsum.photos/id/230/50/50",
       },
     ],
   };
+
   render() {
     return (
       <div className="container-fluid">
@@ -47,30 +51,61 @@ export default class MainContent extends Component {
         </h2>
         <table className="table">
           <thead>
-            <tr>
-              <th>Name</th>
-              <th>School</th>
+            <tr className="">
+              <th>Id</th>
+              <th>Photo</th>
+              <th>Names</th>
               <th>Age</th>
-              <th>school</th>
+              <th>School</th>
             </tr>
           </thead>
-          <tbody>
-            {this.state.customers.map((customer) => {
-              return (
-                <tr key={customer.id}>
-                  <td>{customer.id}</td>
-                  <td>{customer.names}</td>
-                  <td>{customer.age}</td>
-                  <td>{customer.school}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{this.getCustomerRow()}</tbody>
         </table>
       </div>
     );
   }
   onClickRefresh = () => {
     this.setState({ pageBody: this.state.pageTitle });
+  };
+
+  getPhoneToRender = (customer) => {
+    if (customer.age) return customer.age;
+    else {
+      return <div className="bg-warning text-center">No age</div>;
+    }
+  };
+
+  getCustomerRow = () => {
+    return this.state.customers.map((customer, index) => {
+      return (
+        <tr key={customer.id}>
+          <td>{customer.id}</td>
+          <td>
+            <img src={customer.photo} alt="image from the picsum" />
+            <div>
+              <button
+                className="btn btn-info m-2"
+                onClick={() => {
+                  this.onChangePictureClick(customer, index);
+                }}
+              >
+                Change image
+              </button>
+            </div>
+          </td>
+          <td>{customer.names}</td>
+          <td>{this.getPhoneToRender(customer)}</td>
+          <td>{customer.school}</td>
+        </tr>
+      );
+    });
+  };
+
+  onChangePictureClick = (customer, index) => {
+    console.log(customer);
+    let customerArr = this.state.customers;
+    customerArr[index].photo = "https://picsum.photos/id/229/50/50"
+
+    this.setState({ customers: customerArr });
   };
 }
